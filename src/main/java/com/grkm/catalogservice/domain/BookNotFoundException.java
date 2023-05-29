7410d744d2580c70f58581efa56863b26910165a
Gorkem Tanagardigil <119583820+tanagardigil-gorkem@users.noreply.github.com>
2023-05-29 21:26:15 +0200

    comimt-stage added for action

---
+++ src/main/java/com/grkm/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.grkm.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException{
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
---
+++ src/test/java/com/grkm/catalogservice/BookControllerMvcTests.java
@@ -0,0 +1,35 @@
+package com.grkm.catalogservice;
+
+import com.grkm.catalogservice.domain.BookNotFoundException;
+import com.grkm.catalogservice.domain.BookService;
+import com.grkm.catalogservice.web.BookController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request
+        .MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result
+        .MockMvcResultMatchers.status;
+@WebMvcTest(controllers = BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
