7cad66e4d19629654f72a826d496d760dec7aed9
Gorkem Tanagardigil <119583820+tanagardigil-gorkem@users.noreply.github.com>
2023-06-05 22:50:49 +0200

    config server configured

--- src/main/java/com/grkm/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/grkm/catalogservice/CatalogServiceApplication.java
@@ -1,9 +1,12 @@
 package com.grkm.catalogservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
